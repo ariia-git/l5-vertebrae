6f87ff541c117c658ff94f5c81215267225a132f
Nick Menke <tierkonig@hotmail.com>
2017-12-20 19:07:03 -0500

    Fixed DB issue when version doesn't support string length of 255; attempt to add travis-ci

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->handleDbDefaultStringLength();
+
         $locales = app(Locale::class)->getLocalesForConfig();
+
         \Config::set('localization.activeLocales', $locales['active']);
         \Config::set('localization.supportedLocales', $locales['supported']);
         \Config::set('localization.hideDefaultLocaleInUrl', true);
@@ -42,5 +45,35 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->alias('bugsnag.logger', Log::class);
         $this->app->alias('bugsnag.logger', LoggerInterface::class);
+    }
+
+    /**
+     * Limit the default string length setting if the current MySQL / MariaDB version does not support 255.
+     */
+    private function handleDbDefaultStringLength()
+    {
+        // check if defaultStringLength is going to work
+        if (\DB::connection()->getDriverName() == 'mysql') {
+            // check MySQL / MariaDB version
+            $dbVersionQuery = \DB::select(\DB::raw('select version()'));
+
+            if ($dbVersion = reset($dbVersionQuery)->{'version()'}) {
+                if (strpos($dbVersion, 'Maria') !== false) {
+                    // MariaDB
+                    $minDbVersion = '10.2.2';
+
+                    // we just need the version number
+                    $dbVersion = substr($dbVersion, 0, strpos($dbVersion, '-Maria'));
+                } else {
+                    // MySQL
+                    $minDbVersion = '5.7.7';
+                }
+
+                if ($dbVersion < $minDbVersion) {
+                    // default string length needs to be limited
+                    \Schema::defaultStringLength(191);
+                }
+            }
+        }
     }
 }
